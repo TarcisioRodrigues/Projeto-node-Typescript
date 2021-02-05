4acf65b6565af82e68d9721b446fa1978b9557b0
Tarcisio Rodrigues <tarcisiorodrigues454@gmail.com>
2021-02-04 21:49:45 -0300

    editorconfig

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { request, response } from 'express';
 
 const app=express();
 app.get('/',(request,response)=>{
-  return response.json({message:'Hello world'})
+  return response.json({message:'Hello world'});
 })
 app.listen(3333,()=>{
   console.log('Server running')
